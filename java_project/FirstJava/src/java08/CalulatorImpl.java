da27d9c34146b953c5bde5e54038728b9d9faa1d
jian-level-is <myunggeun22@gmail.com>
2021-10-19 15:23:40 +0900

    update

--- java_project/FirstJava/src/java08/CalulatorImpl.java
+++ java_project/FirstJava/src/java08/CalulatorImpl.java
@@ -1,19 +1,6 @@
 package java08;
 
 public abstract class CalulatorImpl implements Calulator{
-	// 추상클래스 정의
-	long n1;
-	long n2;
-	
-	public CalulatorImpl() {
-		n1 = 1;
-		n2 = 2;
-	}
-
-	public CalulatorImpl(long n1, long n2) {
-		this.n1 = n1;
-		this.n2 = n2;
-	}
 
 	@Override
 	public long add(long n1, long n2) {
