7d5bd4c4c165be966df5e24104dcea68b7b23561
jian-level-is <myunggeun22@gmail.com>
2021-11-19 19:57:29 +0900

    update

---
+++ algo/algo/src/bruteForce/BlackJack.java
@@ -0,0 +1,27 @@
+package bruteForce;
+
+/*
+	
+	첫째 줄에 카드의 갯수 N과 	M이 주어진다.
+	N은 카드의 갯수,  N = 4  4개의 랜덤의 숫자가 
+	M은 조합을 통해 만들어진 4개의 숫자가 M을 넘지않으며 가장 큰 수를 출력
+
+	10 500
+	93 181 245 214 315 36 185 138 216 295
+
+*/
+
+public class BlackJack {
+	
+	public static void main(String[] args) {
+		
+		int [] a = {93, 181, 245, 214, 315, 36, 185, 138, 216, 295};
+		jack(a,500);
+	}
+	
+	public static int jack(int [] nums, int maxNum) {
+
+		
+		return 1;
+	}
+}
